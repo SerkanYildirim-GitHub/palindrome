413b699646cf1e1bcd5001a83fd8d473619e8650
SerkanYildirim-GitHub <82678084+SerkanYildirim-GitHub@users.noreply.github.com>
2022-06-05 21:23:11 -0700

    added replace space function to the code.
    added replace function makes the code validate the palindrome sentences as well. 
    Thanks to @Devurandom11

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,30 +1,26 @@
 # a simple code to check if a user input is palindrome or not.
 # Function to check the input and the "reverse of input" equals
+import string
 word = ""
 def isPalindrome(word):
     # Get input word from user to check if it is Palindrome 
-    word = input("Which word would you like to check if it is a palindrome ?") 
-    
+    ask = input("Which word would you like to check if it is a palindrome ?") 
+    # remove spaces if user input is a sentence
+    no_space = ask.replace(' ', '')
+    # convert all letters to lowercase
+    word = no_space.lower()
     if word == word[::-1]:
-        print(f"\nYes! the word \"{word}\" is a palindrome.!\n--------------------------------------------")
+        print(f"\nYes! the word \"{ask}\" is a palindrome.!\n--------------------------------------------")
     else:
-        print(f"\nNo, the word \"{word}\" is NOT a palindrome.!\n--------------------------------------------")
+        print(f"\nNo, the word \"{ask}\" is NOT a palindrome.!\n--------------------------------------------")
     
     word = input("Want to check another word? (y)es / (n)o ")
     
     if word.lower() == "y":
-        isPalindrome(word)
+        isPalindrome(ask)
     elif word.lower() =="n":
         print("Thank you for using our automated palindrome service. Bye! ")
     else:
         print("Invalid selection. BYE!")
         pass
 isPalindrome(word)
-
-
-# Remove spaces from palindrome sentence
-
-sentence = "This is a test sentence"
-print(sentence)
-new_sentence = sentence.replace(' ', '')
-print(new_sentence)
